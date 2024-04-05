1100b150f5b037391f9a258c918931e0f0c71ab5
KrisK <37834268+KristofferKay@users.noreply.github.com>
2024-04-05 09:19:28 +0200

    Added CSV file Added attributes to Covid19Data.java

--- src/Covid19Data.java
+++ src/Covid19Data.java
@@ -1,2 +1,11 @@
 public class Covid19Data {
+
+    String region;
+    String aldersGruppe;
+    int bekræftetTilfælde;
+    int døde;
+    String indlagtePåIntensiv;
+    int indlagte;
+    String dato;
+
 }

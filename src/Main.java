49377abe16af8d37ce18abedede0e6a5c95d4958
jona0001 <jona0001@stud.kea.dk>
2024-04-08 10:32:14 +0200

    sorting

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Main {
     public static void main(String[] args) {
         FileHandler fh = new FileHandler();
-        ArrayList<Covid19Data> covidDataArr = fh.loadAllData();
+        ArrayList<Covid19Data> covidData = fh.loadAllData();
 
-        for (Covid19Data covid19Data : fh.loadAllData()) {
-                System.out.println(covidDataArr);
+        System.out.println("Sorting by Name");
+        Collections.sort(covidData, new RegionComparator());
+        for (Covid19Data covid19Data : covidData) {
+            System.out.println(covid19Data);
         }
+
     }
 }
